4e6dad89600899168d40ee870df99bdbe781b740
Francoismn <113969158+Francoismn@users.noreply.github.com>
2024-05-06 01:08:16 +0200

    liens css et js à faire pour overlay

--- src/main/resources/static/js/common/modal.js
+++ src/main/resources/static/js/common/modal.js
@@ -1,4 +1,14 @@
-const deleteBtnEl = document.getElementById('delete');
-deleteBtnEl.addEventListener('click', (e) => {
-    console.log("delete");
+const displayElement = document.getElementById("delete-btn");
+const deleteConfirm = document.getElementById("delete-confirmation");
+const formProfile = document.getElementById("form-edit-profile");
+
+
+deleteConfirm.style.display = "none";
+console.log("oui");
+displayElement.addEventListener('click', (e) => {
+    deleteConfirm.style.display = "block";
+    formProfile.style.display = "none";
+
+
+
 })
